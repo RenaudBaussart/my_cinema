fe999586e44957511bf89e267785faeff3895a9f
Baussart Renaud <renaud.baussart@proton.me>
2026-01-26 16:41:03 +0100

    feat: add config to gitignore

--- backend/config/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-$dsn = "mysql:host=localhost;dbname=my_cinema;charset=utf8mb4";
-$user = "root";
-$pass = "";
-$pdo = new PDO(dsn: $dsn, username: $user, password: $pass, options: [
-	PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-	PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-]);
-?>
